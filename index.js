8a62d41a85cf5db2258aba26bce8d86062916a81
Salome <salomekaeni@gmail.com>
2024-06-27 10:38:27 -0700

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Christine Kaeni';
+const welcomeMessage = 'Welcome to Flatbook, ${Christine Kaeni}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${C}!'; 
